6174e068150e0ac1c3798eb594780df152b67366
Matheus Maica <matheus_maica@sicredi.com.br>
2023-09-22 15:12:35 -0300

    flexao

--- app.py
+++ app.py
@@ -3,12 +3,40 @@ import mediapipe as mp
 import threading
 import time
 
+def exercicioFlexao(body):
+    global baixoCount, flexCount, cimaFlag
+    nariz = body[0][1]
+
+    if(nariz < linhaHorizonte and not cimaFlag):
+        cimaFlag = True
+        flexCount += 1
+
+    elif(nariz > linhaHorizonte and cimaFlag):
+        cimaFlag = False
+
+def exercicioAbdominal(body):
+    pass
+def tipoExercicio(body):
+    mao = body[16][1]
+    cintura = body[24][1]
+    if(mao > cintura):
+        return "flexao"
+    if(mao < cintura):
+        return "Abdominal"
+    
+    return "desconhecido"
+
 camera = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_PLAIN
 scale = 3
 pTime = 0
 color = (255,0,0)
 thickness = 3
+
+linhaHorizonte = 235
+
+cimaFlag = True
+flexCount = 0
 
 mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
@@ -26,13 +54,20 @@ while True:
                 body.append((cx,cy))
             mpDraw.draw_landmarks(frame, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
 
-        
+        tipo = tipoExercicio(body)
         cTime = time.time()
         fps = 1/(cTime - pTime)
         pTime = cTime
-
+        if(tipo == "flexao"):
+            exercicioFlexao(body)
+        if(tipo == "Abdominal"):
+            exercicioAbdominal(body)
         cv2.putText(frame,str(f"FPS: {int(fps)}"),(0,40),font,scale,color,thickness)
-        cv2.line(frame, (0,235), (700,235), color, 1)
+        cv2.putText(frame,str(f"exercicio: {tipo}"),(0,100),font,scale,color,thickness)
+        cv2.putText(frame,str(f"count: {int(flexCount)}"),(0,140),font,scale,color,thickness)
+
+
+        cv2.line(frame, (0,linhaHorizonte), (700,linhaHorizonte), color, 1)
 
         cv2.imshow("e-gym", frame)
 
